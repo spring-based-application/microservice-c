980c7357d188e3b61a25b22fd813a434d6b1996b
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-05-12 13:22:48 +0200

    First Commit.

---
+++ src/main/java/com/trl/microservicec/MicroserviceCApplication.java
@@ -0,0 +1,13 @@
+package com.trl.microservicec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trl/microservicec/MicroserviceCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trl.microservicec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
